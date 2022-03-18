5ca795e6a3859e95e9c159199fcb6ccf9b0636f3
JennyAdroja <jennycollege2@gmail.com>
2022-03-17 20:06:59 -0400

    commit

--- src/Options.js
+++ src/Options.js
@@ -1,32 +1,32 @@
  import React from 'react'
-
+ import './main.css';
  class Options extends React.Component{
     render(){
         return(
-            <div>
+            <div className='mainMenu'>
                 <div style={{backgroundColor:"#F3F4F7;"}}>
                     <h3>INSTALL PYTORCH</h3>
                     <h5>PyTorch Build</h5>
-                    <input type="checkbox" name="Stabia"/><br>Stabia(1.11.0)</br>
-                    <input type="checkbox" name="Preview"/><br>Preview(Nightly)</br>
-                    <input type="checkbox" name="Lts"/><br>LTS(1.8.2)</br>
+                    <input type="checkbox" name="Stabia"/>Stabia(1.11.0)
+                    <input type="checkbox" name="Preview"/>Preview(Nightly)
+                    <input type="checkbox" name="Lts"/>LTS(1.8.2)
                     <h5>Your OS</h5>
-                    <input type="checkbox" name="Linux"/><br>Linux</br>
-                    <input type="checkbox" name="Mac"/><br>Mac</br>
-                    <input type="checkbox" name="Windows"/><br>Windows</br>
+                    <input type="checkbox" name="Linux"/>Linux
+                    <input type="checkbox" name="Mac"/>Mac
+                    <input type="checkbox" name="Windows"/>Windows
                     <h5>Package</h5>
-                    <input type="checkbox" name="Conda"/><br>Conda</br>
-                    <input type="checkbox" name="Pip"/><br>Pip</br>
-                    <input type="checkbox" name="Libtorch"/><br>LibTorch</br>
-                    <input type="checkbox" name="Source"/><br>Source</br>
+                    <input type="checkbox" name="Conda"/>Conda
+                    <input type="checkbox" name="Pip"/>Pip
+                    <input type="checkbox" name="Libtorch"/>LibTorch
+                    <input type="checkbox" name="Source"/>Source
                     <h5>Language</h5>
-                    <input type="checkbox" name="Python"/><br>Preview(Nightly)</br>
-                    <input type="checkbox" name="CJava"/><br>C++/Java</br>
+                    <input type="checkbox" name="Python"/>Preview(Nightly)
+                    <input type="checkbox" name="CJava"/>C++/Java
                     <h5>Compute Plateform</h5>
-                    <input type="checkbox" name="Cudaten"/><br>CUDA 10.2</br>
-                    <input type="checkbox" name="Cuda"/><br>CUDA 11.3</br>
-                    <input type="checkbox" name="Rocm"/><br>ROCM 4.2(beta)</br>
-                    <input type="checkbox" name="Cpu"/><br>CPU</br>
+                    <input type="checkbox" name="Cudaten"/>CUDA 10.2
+                    <input type="checkbox" name="Cuda"/>CUDA 11.3
+                    <input type="checkbox" name="Rocm"/>ROCM 4.2(beta)
+                    <input type="checkbox" name="Cpu"/>CPU
                     <div style={{backgroundColor:"gray;",color:"black;"}}></div>
                 </div>
             </div>
